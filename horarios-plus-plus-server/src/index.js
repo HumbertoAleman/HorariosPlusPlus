e51dda1d62ac9bd1f035f8df4530d154290b31fc
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2024-05-18 18:59:20 -0400

    Created index.js

---
+++ horarios-plus-plus-server/src/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from "cors"
+import mongoose from 'mongoose';
+
+import subjectRoutes from './routes/subject/subject.routes.js';
+
+const app = express()
+const port = 4000
+
+const uri = "mongodb+srv://humberto:cocosete@serverdata.64ryvhh.mongodb.net/?retryWrites=true&w=majority&appName=serverdata";
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+async function run() {
+  await mongoose.connect(uri, clientOptions);
+  await mongoose.connection.db.admin().command({ ping: 1 });
+  console.log("Successfully connected to MongoDB");
+}
+
+run().catch(console.dir);
+
+subjectRoutes(app)
+
+app
+  .use(cors({ origin: "*" }))
+  .listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
