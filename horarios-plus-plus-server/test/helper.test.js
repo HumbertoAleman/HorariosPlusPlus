0eb54b3f7b83d183d55714f632bccef506c1c163
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2024-06-11 21:38:47 -0400

    Tests should be working again

--- horarios-plus-plus-server/test/helper.test.js
+++ horarios-plus-plus-server/test/helper.test.js
@@ -1,5 +1,8 @@
 import mongoose from 'mongoose'
 import Subject from '../src/models/subject.model.js'
+import Section from '../src/models/section.model.js'
+import Session from '../src/models/session.model.js'
+import User from '../src/models/user.model.js'
 
 mongoose.Promise = global.Promise
 const MONGODB_URI = 'mongodb+srv://humberto:cocosete@serverdata.64ryvhh.mongodb.net/?retryWrites=true&w=majority&appName=serverdata'
@@ -13,8 +16,14 @@ mongoose.connection
 
 beforeEach(async () => {
 	await Subject.dropDb()
+	await Section.dropDb()
+	await Session.dropDb()
+	await User.dropDb()
 })
 
 after(() => {
 	Subject.dropDb()
+	Section.dropDb()
+	Session.dropDb()
+	User.dropDb()
 })
