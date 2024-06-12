98d00b4787b7b415c851adb367450a6d6a2a4640
HumbertoAleman <haleman.23@est.ucab.edu.ve>
2024-06-11 20:14:21 -0400

    Section updated

--- horarios-plus-plus-server/src/models/section.model.js
+++ horarios-plus-plus-server/src/models/section.model.js
@@ -1,12 +1,174 @@
 import mongoose from "mongoose"
+import Subject from "./subject.model.js"
+import SessionController from "../routes/session/SessionController.js"
+import Schedule from "./schedule.model.js"
+const ObjectId = mongoose.Types.ObjectId
 
-const SectionSchema = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  nrc: { type: String, require: true, unique: true },
-  teacher: { type: String, require: true, unique: false },
-  sessions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Session" }],
-  subject: { type: mongoose.Schema.Types.ObjectId, require: true, ref: "Subject" }
-})
+export default class Section {
+	static #schema = new mongoose.Schema({
+		_id: mongoose.Schema.Types.ObjectId,
+		nrc: { type: Number, require: true, unique: true },
+		teacher: { type: String, require: true, unique: false },
+		sessions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Session" }],
+		subject: { type: mongoose.Schema.Types.ObjectId, require: true, ref: "Subject" }
+	})
 
-const Section = mongoose.model("Section", SectionSchema) 
-export default Section
+	static #model = mongoose.model("Section", Section.#schema)
+
+	static find = async (nrc) => await Section.#model.find({ nrc: nrc })
+	static findOne = async (nrc) => await Section.#model.findOne({ nrc: nrc })
+	static findById = async (id) => await Section.#model.findById(id)
+
+	static findAndUpdate = async (oldNrc, newData) =>
+		await Section.#model.findOneAndUpdate(
+			{ nrc: oldNrc }, newData, { new: true })
+
+	static findAndDelete = async (nrc) =>
+		await Section.#model.findOneAndDelete({ nrc: nrc })
+	static findByIdAndDelete = async (id) =>
+		await Section.#model.findByIdAndDelete(id)
+
+	static countSections = async () => await Section.#model.find().countDocuments() + 1 ?? 1
+	static checkIfExists = async (nrc) => await Section.#model.exists({ nrc: nrc })
+	static checkIfIdExists = async (id) => await Section.#model.findById(id).then(res => res !== null)
+
+	static byIdAddSession = async (sectionId, sessionId) =>
+		await Section.#model.findByIdAndUpdate(
+			sectionId, {
+			sessions: await Section.findById(sectionId).then(res =>
+				res.sessions.concat(new ObjectId(sessionId)))
+		}, { new: true })
+
+	static byNrcAddSession = async (sectionNrc, sessionId) =>
+		await Section.#model.findOneAndUpdate(
+			{ name: sectionNrc }, {
+			sessions: await Section.findOne(sectionNrc).then(res =>
+				res.sessions.concat(new ObjectId(sessionId)))
+		}, { new: true })
+
+	static byIdRemoveSession = async (sectionId, sessionId) =>
+		await Section.#model.findByIdAndUpdate(
+			sectionId, {
+			sessions: await Section.findById(sectionId).then(res =>
+				res.sessions.filter(otherId => !otherId.equals(sessionId)))
+		}, { new: true })
+
+	static byNrcRemoveSession = async (sectionNrc, sessionId) =>
+		await Section.#model.findOneAndUpdate(
+			{ name: sectionNrc }, {
+			sessions: await Section.findOne(sectionNrc).then(res =>
+				res.sessions.filter(otherId => !otherId.equals(sessionId)))
+		}, { new: true })
+
+	static dropDb = async () => await mongoose.connection.collections.sections.drop()
+
+	// NOTE: CREATE
+	static save = async (nrc, teacher, sessions, subject) => {
+		if (subject === undefined)
+			return { message: "ERROR subject is undefined", code: 0 }
+		if (await Subject.checkIfExists(subject).then(res => !res))
+			return { message: "ERROR Subject does not exist", code: 0 }
+
+		if (await Section.checkIfExists(nrc))
+			return { message: "ERROR nrc already exists", code: 0 }
+
+		const newSection = new Section.#model({
+			_id: new ObjectId(),
+			nrc: nrc ?? await Section.countSections().then(res => parseInt(res)),
+			teacher: teacher ?? "Por Asignar",
+			sessions: sessions?.split(',') ?? [],
+			subject: await Subject.findOne(subject).then(res => new ObjectId(res._id))
+		})
+		const createdSection = await newSection.save()
+
+		if (await Section.checkIfExists(createdSection.nrc)) {
+			await Subject.byNameAddSection(subject, createdSection._id)
+			return createdSection
+		}
+
+		return { message: "ERROR saving section", code: 0 }
+	}
+
+
+	// NOTE: READ
+	static getFromSubject = async (subjectId) => {
+		if (nrc === undefined)
+			return []
+		const sections = []
+		for (const sectionId of await Subject.findById(subjectId).then(res => res.sections))
+			sections.push(await Section.findById(sectionId))
+	}
+
+	static getByNrc = async (nrc) => {
+		if (nrc === undefined)
+			return { message: "ERROR section with nrc " + nrc + " was not found", code: 0 }
+		return await Section.findOne(nrc)
+	}
+
+	static getById = async (id) => {
+		if (id === undefined)
+			return { message: "ERROR section with id " + id + " was not found", code: 0 }
+		return await Section.findById(id)
+	}
+
+	// NOTE: UPDATE
+	static update = async (oldNrc, newNrc, newTeacher) => {
+		const newData = {}
+
+		if (oldNrc === undefined)
+			return { message: "ERROR oldNrc is undefined", code: 0 }
+		if (await Section.checkIfExists(oldNrc).then(res => !res))
+			return { message: "ERROR section with nrc " + oldNrc + " was not found", code: 0 }
+
+		if (newNrc !== undefined)
+			newData.nrc = newNrc
+		if (newTeacher !== undefined)
+			newData.teacher = newTeacher
+
+		const updatedSection = await Section.findAndUpdate(oldNrc, newData)
+		if (updatedSection === undefined || updatedSection === null)
+			return { message: "ERROR section with nrc " + oldNrc + " was not found", code: 0 }
+		return updatedSection
+	}
+
+	// NOTE: DELETE
+	static #editAffected = async (sectionObject) => {
+		Subject.byIdRemoveSection(sectionObject.subject, deletedData._id)
+		for (const sessionId of sectionObject.sessions)
+			await SessionController.deleteSession({ query: { id: sessionId } })
+		await Schedule.deleteMany({ sections: sectionObject._id })
+	}
+
+	static delete = async (nrc) => {
+		if (nrc === undefined)
+			return { message: "ERROR nrc is undefined", code: 0 }
+
+		if (await Section.checkIfExists(nrc).then(res => !res))
+			return { message: "ERROR there is no section with nrc " + nrc, code: 0 }
+
+		const deletedData = await Section.findAndDelete(nrc)
+		if (deletedData === undefined)
+			return { message: "ERROR an error has occurred deleting the section", code: 0 }
+
+		Section.#editAffected(deletedData)
+
+		return deletedData
+	}
+
+	static deleteById = async (id) => {
+		if (id === undefined)
+			return { message: "ERROR id is undefined", code: 0 }
+
+		if (await Section.checkIfIdExists(id).then(res => !res))
+			return { message: "ERROR there is no section with id " + id, code: 0 }
+
+		const deletedData = await Section.findByIdAndDelete(nrc)
+		if (deletedData === undefined)
+			return { message: "ERROR an error has occurred deleting the section", code: 0 }
+
+		Section.#editAffected(deletedData)
+
+		return deletedData
+	}
+}
+
--- horarios-plus-plus-server/test/section.test.js
+++ horarios-plus-plus-server/test/section.test.js
@@ -1,205 +1,60 @@
-import newSection from "../src/routes/section/section.new_section.js"
-import newSubject from "../src/routes/subject/subject.new_subject.js"
-import deleteSection from "../src/routes/section/section.delete_section.js"
-import getSection from "../src/routes/section/section.get_section.js"
-import getSections from "../src/routes/section/section.get_sections.js"
-
-import Section from "../src/models/section.model.js"
 import assert from "assert"
-
-import mongoose from "mongoose"
 import Subject from "../src/models/subject.model.js"
-import updateSection from "../src/routes/section/section.update_section.js"
+import SubjectController from "../src/routes/subject/SubjectController.js"
+import Section from "../src/models/section.model.js"
+import SectionController from "../src/routes/section/SectionController.js"
 
 describe("Section CRUD", () => {
 	let subject
 	beforeEach(async () => {
-		subject = await newSubject({ query: { name: "Subject" } });
+		subject = await SubjectController.newSubject()
 	})
 
-	// NOTE: CREATE SECTIONS
+	// NOTE: CREATE SUBJECTS 
 	describe("Create Sections", () => {
 		it("Create a new section with no arguments", async () => {
-			await newSection({
-				query: { subjectName: subject.name }
-			})
-
-			assert(await Section.exists({
-				nrc: 0,
-				teacher: "Por Asignar",
-				sessions: [],
-				subject: new mongoose.mongo.ObjectId(subject._id)
-			}).then(res => res !== null))
-		})
-
-		it("Ensure that section is in subject when created", async () => {
-			let createdSection = await newSection({
-				query: { subjectName: subject.name }
-			})
-
-			assert(await Subject
-				.findById({ _id: new mongoose.mongo.ObjectId(subject._id) })
-				.then(res =>
-					res.sections.some(id => id.equals(new mongoose.mongo.ObjectId(createdSection._id)))
-				))
-		})
-
-		it("Create a new section with no arguments, with sections already in db", async () => {
-			for (let i = 0; i < 3; i++) {
-				await newSection({
-					query: {
-						nrc: i,
-						subjectName: subject.name
-					}
-				})
-			}
-
-			assert(await Section.exists({
-				nrc: 2,
-				teacher: "Por Asignar",
-				sessions: [],
-				subject: new mongoose.mongo.ObjectId(subject._id)
-			}))
-		})
-
-		it("Create new section with nrc and teacher", async () => {
-			await newSection({
-				query: {
-					nrc: 21,
-					teacher: "Test Teacher",
-					subjectName: subject.name
-				}
-			})
-
-			assert(await Section.exists({
-				nrc: 21,
-				teacher: "Test Teacher",
-				sessions: [],
-				subject: new mongoose.mongo.ObjectId(subject._id)
-			}))
-		})
-
-		it("Throw when adding duplicate NRCs", async () => {
-			await newSection({
-				query: { nrc: 21, subjectName: subject.name }
-			})
-			assert(await newSection({
-				query: { nrc: 21, subjectName: subject.name }
-			}).then(res => res.message === "ERROR Duplicate NRC"))
-		})
-
-		it("Throw when subject name is undefined", async () => {
-			assert(await newSection({
-				query: { nrc: 21 }
-			}).then(res => res.message === "ERROR Subject name is undefined"))
-		})
-
-		it("Throw when adding to a section that doesn't exist", async () => {
-			assert(await newSection({
-				query: { nrc: 21, subjectName: "Oogly oo" }
-			}).then(res => res.message === "ERROR Subject does not exist"))
+			const section = await SectionController.newSection({ query: { subjectName: subject.name } })
+			assert(await Section.checkIfExists(section.nrc) && await Subject.hasSection(subject._id, section._id))
 		})
 	})
 
-	// NOTE: READ SECTIONS
-	describe("Read Sections", () => {
+	// NOTE: READ SUBJECTS 
+	describe("Get Sections", () => {
 		let toRead
 		beforeEach(async () => {
-			toRead = await newSection({ query: { nrc: "1", subjectName: subject.name } })
+			toRead = await SectionController.newSection({ query: { subjectName: subject.name } })
 		})
 
-		it("Get section using nrc", async () => {
-			assert(await getSection({ query: { nrc: toRead.nrc } })
-				.then(res => res.nrc === toRead.nrc))
+		it("Read a section (ID)", async () => {
+			assert(await SectionController.getSection({ query: { id: toRead._id } }).then(res => res._id.equals(toRead._id)))
 		})
 
-		it("Throw when nrc is undefined", async () => {
-			assert(await getSection({ query: { nrc: undefined } })
-				.then(res => res.message === "ERROR sectionNRC is undefined"))
-		})
-
-		it("Throw when nrc is not found", async () => {
-			assert(await getSection({ query: { nrc: "32" } })
-				.then(res => res.message === "ERROR section with nrc 32 was not found"))
-		})
-
-		it("Get sections from a subject", async () => {
-			await newSection({ query: { nrc: "2", subjectName: subject.name } })
-			await newSection({ query: { nrc: "3", subjectName: subject.name } })
-			assert(await getSections({ query: { subjectName: subject.name } })
-				.then(res => res.every(section => ["1", "2", "3"].includes(section.nrc))))
-		})
-
-		it("Throw when subjectName is undefined", async () => {
-			assert(await getSections({ query: { subjectName: undefined } })
-				.then(res => res.message === "ERROR subjectName is undefined"))
-		})
-
-		it("Throw when subject is not found", async () => {
-			assert(await getSections({ query: { subjectName: "MATERIA DOS" } })
-				.then(res => res.message === "ERROR subject does not exist"))
+		it("Read a section (NRC)", async () => {
+			assert(await SectionController.getSection({ query: { nrc: toRead.nrc } }).then(res => res._id.equals(toRead._id)))
 		})
 	})
 
-	// NOTE: UPDATE SECTIONS
-	describe("Update Sections", () => {
+	// NOTE: UPDATE SUBJECTS 
+	describe("Update a Section", () => {
 		let toUpdate
 		beforeEach(async () => {
-			toUpdate = await newSection({ query: { nrc: "0", subjectName: subject.name } })
+			toUpdate = await SectionController.newSection({ query: { subjectName: subject.name } })
 		})
 
-		it("Update section nrc", async () => {
-			assert(await updateSection({ query: { oldNrc: "0", newNrc: "1" } })
-				.then(res => res._id.equals(toUpdate._id) && res.nrc === "1"))
-		})
-
-		it("Update section teacher", async () => {
-			assert(await updateSection({ query: { oldNrc: "0", newTeacher: "Gustav" } })
-				.then(res => res._id.equals(toUpdate._id) && res.teacher === "Gustav"))
-		})
-
-		it("Throw when newSection and newTeacher are both undefined", async () => {
-			assert(await updateSection({ query: { oldNrc: "0" } })
-				.then(res => res.message === "ERROR newNrc and newTeacher are both undefined"))
-		})
-
-		it("Throw if oldNrc is undefined", async () => {
-			assert(await updateSection({ query: { oldNrc: undefined, newNrc: "1" } })
-				.then(res => res.message === "ERROR oldNrc is undefined"))
-		})
-
-		it("Throw if no section is found", async () => {
-			assert(await updateSection({ query: { oldNrc: "32", newNrc: "1" } })
-				.then(res => res.message === "ERROR section with nrc 32 was not found"))
+		it("Update a section", async () => {
+			assert(await SectionController.updateSection({ query: { oldNrc: toUpdate.nrc, newNrc: 32, newTeacher: "Juan" } }).then(res => res._id.equals(toUpdate._id) && res.nrc === 32 && res.teacher === "Juan"))
 		})
 	})
 
-	// NOTE: DELETE SECTIONS
-	describe("Delete Sections", () => {
+	// NOTE: DELETE SUBJECTS 
+	describe("Delete a Section", () => {
 		let toDelete
 		beforeEach(async () => {
-			toDelete = await newSection({ query: { nrc: 0, subjectName: subject.name } })
+			toDelete = await SectionController.newSection({ query: { subjectName: subject.name } })
 		})
 
-		it("Delete section by NRC", async () => {
-			assert(await deleteSection({
-				query: {
-					nrc: 0,
-				}
-			}).then(res => res._id.equals(toDelete._id)))
-		})
-
-		it("Delete section and ensure that its removed from subject", async () => {
-			await deleteSection({ query: { nrc: 0, } })
-			assert(await Subject.findById(subject._id)
-				.then(res => !res.sections.some(id => id.equals(toDelete._id))))
-		})
-
-		it("Throw when deleting section that doesnt exist", async () => {
-			assert(await deleteSection({
-				query: { nrc: 1, }
-			}).then(res => res.message === "ERROR Section does not exist"))
+		it("Delete a section", async () => {
+			assert(await SectionController.deleteSection({ query: { nrc: toDelete.nrc } }).then(res => Section.checkIfExists(res.nrc).then(res => !res)))
 		})
 	})
 })
-
