3021717a78aa8ded8101f1819b9789b01a7fd68a
Kimbix <alemanhumberto06@gmail.com>
2024-06-26 15:31:45 -0400

    Started working on event page

--- horarios-plus-plus/src/Pages/Events/Events.tsx
+++ horarios-plus-plus/src/Pages/Events/Events.tsx
@@ -2,18 +2,52 @@ import "./Events.css"
 
 import React from "react"
 import SplitLayout from "../../Layouts/Split/SplitLayout.tsx"
+import IEvent from "../../CommonComponents/Interfaces/IEvent.ts"
 
-export default function Event()  {
+const randomEvent = () => {
+  console.log("RANDOM")
+  return {
+    name: "Test event " + Math.floor(Math.random() * 100),
+    day: 0,
+    start: {
+      hour: 0,
+      minute: 0,
+    },
+    end: {
+      hour: 0,
+      minute: 0
+    }
+  }
+}
+
+export default function Event() {
+
+  const [loadedEvents, setLoadedEvents] = React.useState(Array.from(Array(32).keys()).map(_ => randomEvent()))
+  const [selectedEvent, setSelectedEvents] = React.useState<IEvent | undefined>(undefined)
+
+
+  const LEventContainer = (data: IEvent) => (
+    <div>
+      {data.name}
+    </div>
+  )
+
   return (
     <SplitLayout>
       {/* LEFT */}
-      <div>
-        Here goes events
+      <div className="left-event-container">
+        { loadedEvents.map(e => LEventContainer(e)) }
       </div>
 
       {/* RIGHT */}
       <div>
-        Here also goes events
+        {
+          selectedEvent === undefined
+          ? <></>
+          : <div>
+            An event has been selected
+          </div>
+        }
       </div>
     </SplitLayout>
   )
